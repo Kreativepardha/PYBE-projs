62160059bf871788a3719dc19591ffa62cbf6600
saradhi <saradhi@PardhaSaradhi>
2024-09-08 21:22:00 +0530

    signup

---
+++ app/database.py
@@ -0,0 +1,12 @@
+from prisma import Prisma 
+
+db = Prisma()
+
+
+
+async def connect_db():
+    await db.connect()
+
+
+async def disconnect_db():
+    await db.disconnect()
---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends, HTTPExceptions, status
+from app.models import UserCreate, UserLogin, PostCreate
+from prisma import Prisma
+
+router = APIRouter()
+db = Prisma()
+
+@router.post("/signup")
+async def signup(user_data: UserCreate):
+    user = await db.user.find_unique(where={"email": user_data/email})
+    if user: 
+        raise HTTPException(status_code=400, detail="email already regsitered")
+
+    await db.user.create(data={"email": user_data.email, "password":user_data.password})
+    return {"msg": "user created Succesffully"}
